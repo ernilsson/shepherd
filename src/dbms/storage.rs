a9c0e7d391a5939fb7064f0921c4728b200d886f
erasmusnilsson <erasmusnilsson@proton.me>
2026-02-07 07:19:57 -0500

    storage: Adds the next storage operation primitive, write_page.

--- src/dbms/storage.rs
+++ src/dbms/storage.rs
@@ -2,7 +2,7 @@ pub mod ethemeral;
 
 use std::{
     fs::File,
-    io::{self, Read, Seek},
+    io::{self, Read, Seek, Write},
 };
 
 pub const PAGE_SIZE: usize = 8192;
@@ -12,8 +12,18 @@ pub fn read_page(file: &mut File, page: u64, buf: &mut [u8; PAGE_SIZE]) -> io::R
     file.read(buf).map(|_| ())
 }
 
+pub fn write_page(file: &mut File, page: u64, buf: &[u8; PAGE_SIZE]) -> io::Result<()> {
+    let max = file.metadata()?.len() / PAGE_SIZE as u64;
+    if page > max {
+        return Err(io::Error::other("tried to write distant page"));
+    }
+    file.seek(io::SeekFrom::Start(page * PAGE_SIZE as u64))?;
+    file.write_all(buf).map(|_| ())
+}
+
 #[cfg(test)]
 mod tests {
+    use core::panic;
     use std::io::Write;
 
     use super::*;
@@ -32,6 +42,43 @@ mod tests {
             assert_eq!(read_buffer, [5u8; PAGE_SIZE]);
             read_page(tmp.borrow_mut(), 1, &mut read_buffer).unwrap();
             assert_eq!(read_buffer, [9u8; PAGE_SIZE]);
+        });
+    }
+
+    #[test]
+    fn write_page_seeks_multiple_of_page_size() {
+        ethemeral::file!(tmp {
+            let write_buffer = [1u8; PAGE_SIZE];
+            assert!(write_page(tmp.borrow_mut(), 0, &write_buffer).is_ok());
+
+            let write_buffer = [2u8; PAGE_SIZE];
+            assert!(write_page(tmp.borrow_mut(), 1, &write_buffer).is_ok());
+
+            tmp.borrow_mut().seek(io::SeekFrom::Start(0)).unwrap();
+            let mut read_buffer = [0u8; PAGE_SIZE * 2];
+            tmp.borrow_mut().read_exact(&mut read_buffer).unwrap();
+            assert_eq!(read_buffer[0..PAGE_SIZE], [1u8; PAGE_SIZE]);
+            assert_eq!(read_buffer[PAGE_SIZE..PAGE_SIZE*2], [2u8; PAGE_SIZE]);
+        });
+    }
+
+    #[test]
+    fn write_page_given_distant_page() {
+        ethemeral::file!(tmp {
+            // Given *slightly* distant page.
+            let write_buffer = [1u8; PAGE_SIZE];
+            match write_page(tmp.borrow_mut(), 1, &write_buffer) {
+                Ok(_) => panic!("allowed writing distant page"),
+                Err(error) => assert_eq!("tried to write distant page", error.to_string()),
+        }
+        });
+        ethemeral::file!(tmp {
+            // Given *very* distant page.
+            let write_buffer = [1u8; PAGE_SIZE];
+            match write_page(tmp.borrow_mut(), 4, &write_buffer) {
+                Ok(_) => panic!("allowed writing distant page"),
+                Err(error) => assert_eq!("tried to write distant page", error.to_string()),
+        }
         });
     }
 }
